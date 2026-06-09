fc214503598ee077b6d789824118752fe376b39f
Cristian Camilo Diaz Ramirez Estudiante <ccdiaz@estudiante.uniajc.edu.co>
2026-06-09 10:42:10 -0500

    fix: add missing Carbon import in Vehiculo model

--- app/Models/Vehiculo.php
+++ app/Models/Vehiculo.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 use Spatie\Activitylog\Models\Concerns\LogsActivity;
 use Spatie\Activitylog\Support\LogOptions;
 
